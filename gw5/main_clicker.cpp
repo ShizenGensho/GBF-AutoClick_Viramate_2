710d2149358cb6390f00af3cdb3cae59d78c2831
Dustmaner <erick.soy@hotmail.com>
2019-01-26 09:44:21 -0600

    Created .h file

--- gw5/main_clicker.cpp
+++ gw5/main_clicker.cpp
@@ -1,53 +1,10 @@
-// gw5.cpp : Defines the entry point for the console application.
+// main_clicker.cpp : Defines the entry point for the console application.
 //
 
 #include "stdafx.h"
-#include <iostream>
-#include <iomanip>
-#include <windows.h>
-#include <conio.h>
-#include <fstream>
-#include <string>
-#include <stdlib.h> 
-#include <time.h>       
+#include "movement.h"
 
 using namespace std;
-
-
-int randCustom(int delta) {
-	return rand() % (delta * 2) - delta;
-}
-int randX() {
-	return randCustom(12);
-}
-int randY() {
-	return randCustom(12);
-}
-int randD() {
-	return randCustom(80);
-}
-
-void mouseClick(int sleepTime, int variationD, int variationX, int variationY) {
-	mouse_event(MOUSEEVENTF_LEFTDOWN | MOUSEEVENTF_LEFTUP, 0, 0, 0, 0);
-	Sleep(sleepTime + variationD);
-	cout << "X:" << variationX << ",\tY:" << variationY << ",\tTime(ms):" << variationD << "\n";
-}
-
-void swipeLeft(int offsetX, int variationX, int offsetY, int variationY, int sleepTime, int variantonD) {
-	cout << "ID: Swipe Left\n";
-	variationY = randCustom(8);
-	variationX = randCustom(10);
-	SetCursorPos(18 + offsetX + variationX / 2, 633 + offsetY + variationY);//left
-	mouseClick(sleepTime, variantonD, variationY, variationX);
-}
-
-void swipeRight(int offsetX, int variationX, int offsetY, int variationY, int sleepTime, int variantonD) {
-	cout << "ID: Swipe Right\n";
-	variationY = randCustom(8);
-	variationX = randCustom(10);
-	SetCursorPos(460 + offsetX + variationX / 2, 633 + offsetY + variationY);//right
-	mouseClick(sleepTime, variantonD, variationY, variationX);
-}
 
 void main()
 {
---
+++ gw5/movement.h
@@ -0,0 +1,44 @@
+#pragma once
+#include <iostream>
+#include <iomanip>
+#include <windows.h>
+#include <conio.h>
+#include <fstream>
+#include <string>
+#include <stdlib.h> 
+#include <time.h>    
+
+int randCustom(int delta) {
+	return rand() % (delta * 2) - delta;
+}
+int randX() {
+	return randCustom(12);
+}
+int randY() {
+	return randCustom(12);
+}
+int randD() {
+	return randCustom(80);
+}
+
+void mouseClick(int sleepTime, int variationD, int variationX, int variationY) {
+	mouse_event(MOUSEEVENTF_LEFTDOWN | MOUSEEVENTF_LEFTUP, 0, 0, 0, 0);
+	Sleep(sleepTime + variationD);
+	cout << "X:" << variationX << ",\tY:" << variationY << ",\tTime(ms):" << variationD << "\n";
+}
+
+void swipeLeft(int offsetX, int variationX, int offsetY, int variationY, int sleepTime, int variantonD) {
+	cout << "ID: Swipe Left\n";
+	variationY = randCustom(8);
+	variationX = randCustom(10);
+	SetCursorPos(18 + offsetX + variationX / 2, 633 + offsetY + variationY);//left
+	mouseClick(sleepTime, variantonD, variationY, variationX);
+}
+
+void swipeRight(int offsetX, int variationX, int offsetY, int variationY, int sleepTime, int variantonD) {
+	cout << "ID: Swipe Right\n";
+	variationY = randCustom(8);
+	variationX = randCustom(10);
+	SetCursorPos(460 + offsetX + variationX / 2, 633 + offsetY + variationY);//right
+	mouseClick(sleepTime, variantonD, variationY, variationX);
+}
